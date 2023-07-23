1d9cc392480e4114d52b817b484f34e5291608de
Wifqo Arova Syams <56685562+ArovaSyams@users.noreply.github.com>
2023-07-23 08:46:51 +0300

    show the specified car image through Imagin API, add a model & manufacturer search functionality and make a server-side rendering for search

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,11 @@
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
   try {
-    
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const { manufacturer, year, fuel, limit, model } = filters;
+
+    const response = await fetch(
+      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&fuel=${fuel}&limit=${limit}&model=${model}`, {
       headers: {
         'X-RapidAPI-Key': 'f75eaede86mshb5fcd25ac984d62p112e01jsn1c5aad6252d1',
 		    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
@@ -32,3 +35,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', `${make}`);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+}
